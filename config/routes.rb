6b0a6437052963573725b69ab55e3a54e38422ce
thetinahang <thetinahang@gmail.com>
2020-03-08 13:39:19 -0700

    edit routes for nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :key_terms
-  resources :products
+  # resources :key_terms
+  # resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
+
+  scope '/api' do
+  	resources :products do
+  		resources :key_terms
+  	end
+  end
 end
