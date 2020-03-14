3794c79a5400febcbdef9e296d69e3f757cceaea
thetinahang <thetinahang@gmail.com>
2020-03-13 19:02:43 -0700

    change up the grid

--- client/src/containers/ProductList.js
+++ client/src/containers/ProductList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'; 
+import {Box, Grid, Paper } from '@material-ui/core';
 
 class ProductList extends Component {
  	listProducts = () => {
  		console.log('from list products in product list')
  		console.log(this.props.products)
  		return this.props.products['products'].map(product => 
- 			<li key={product['attributes']['id']}>
- 				{product['attributes']['id']}. {product['attributes']['name']} - {product['attributes']['description']}
- 			</li>
+      <Grid container item xs={12} spacing={3}>
+        <Paper>
+		 			<p key={product['attributes']['id']}>
+		 				{product['attributes']['id']}. {product['attributes']['name']} - {product['attributes']['description']}
+		 			</p>
+        </Paper>
+      </Grid> 		
  		)
  	}
 
