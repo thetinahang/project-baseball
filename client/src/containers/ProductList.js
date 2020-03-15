7bd2a09992623d05712e6c20515e9bb53683fd0a
thetinahang <thetinahang@gmail.com>
2020-03-15 12:16:58 -0700

    change card and grid again

--- client/src/containers/ProductList.js
+++ client/src/containers/ProductList.js
@@ -6,52 +6,60 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
+import { Divider } from '@material-ui/core';
+import CardHeader from '@material-ui/core/CardHeader';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    flexGrow: 1,
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  card: {
-    padding: '10px',
-    textAlign: 'center',
-    color: '#00897b',
-  }
-});
+const useStyles = makeStyles(theme => ({
+	root: {
+		borderRadius: 12,
+		minWidth: 256,
+		textAlign: 'center',
+		margin: theme.spacing(2),
+	},
+	header: {
+		textAlign: 'center',
+		spacing: 10,
+	},
+	list: {
+		padding: '20px',
+	},
+	button: {
+		margin: theme.spacing(1),
+	},
+	action: {
+		display: 'flex',
+		justifyContent: 'space-around',
+	},
+}));
 
 function ProductList(products) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  console.log('from product list render')
-    return (
-      <div className={classes.root}>
-        <Grid container direction="row" justify="center" alignItems="center" >
-          {products['products'].map(product => 
-            <Grid item>
-              <Card className={classes.card} variant="outlined" key={product['attributes']['id']}>
-                <CardContent className={classes.root}>
-                  <Typography variant="h5" component="h2">
-                    {product['attributes']['name']}
-                  </Typography>
-                  <Typography variant="body2" component="p">
-                    {product['attributes']['description']}
-                  </Typography>
-                </CardContent>
-                <CardActions style={{justifyContent: 'center'}}>
-                  <Button size="small">See More Info</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          )}
-        </Grid>
-      </div>
-  )
+	console.log('from product list render')
+	return (
+		<Grid container direction="row" justify="center" alignItems="center" >
+			{products['products'].map(product =>
+				<Card className={classes.root} width="400px" >
+					<CardHeader title="" className={classes.header} />
+					<Divider variant="middle" />
+					<CardContent>
+						<Typography variant="h4" align="center">
+							{product['attributes']['name']}
+						</Typography>
+						<div className={classes.list}>
+							<Typography align="center">{product['attributes']['description']}</Typography>
+						</div>
+					</CardContent>
+					<Divider variant="middle" />
+					<CardActions className={classes.action}>
+						<Button variant="contained" color="primary" className={classes.button}>
+							See more info
+						</Button>
+					</CardActions>
+				</Card>
+			)}
+		</Grid>
+	);
 }
 
 export default ProductList; 
