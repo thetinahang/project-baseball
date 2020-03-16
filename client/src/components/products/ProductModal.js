99a869d5b509bf3c19132311a755ca64ad445bf8
thetinahang <thetinahang@gmail.com>
2020-03-15 19:31:48 -0700

    change to fade modal

--- client/src/components/products/ProductModal.js
+++ client/src/components/products/ProductModal.js
@@ -2,26 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import {Button} from '@material-ui/core';
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   paper: {
-    position: 'absolute',
-    width: 400,
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
@@ -31,7 +21,6 @@ const useStyles = makeStyles(theme => ({
 
 export default function ProductModal(product) {
 	const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -51,17 +40,23 @@ export default function ProductModal(product) {
          See More Info
       </Button>
       <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
         open={open}
         onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
       >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">{product.product.name}</h2>
-          <p id="simple-modal-description">
-            {product.product.description}
-          </p>
-        </div>
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">{product.product.name}</h2>
+            <p id="transition-modal-description">{product.product.description}</p>
+          </div>
+        </Fade>
       </Modal>
     </div>
   );
