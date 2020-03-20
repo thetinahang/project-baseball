ad0a2cc1579b8177ce5bd13197b86ae9412ac725
thetinahang <thetinahang@gmail.com>
2020-03-19 19:29:38 -0700

    get navbar working

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { NavLink} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import PBMenu from './PBMenu';
 
-const NavBar = () => {
-	return (
-		<div>
-			<div className="navbar">
-				<NavLink to="/" exact>Home</NavLink>
-				<NavLink to="/products" exact>Products</NavLink>
-			</div>
-		</div>
-	);
-};
 
-export default NavBar;
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <PBMenu/>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ client/src/components/PBMenu.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import { NavLink } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function PBMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <MenuIcon />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
