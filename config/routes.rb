db929d4bca2a462fed2bd0feeb56dc60c8066d6a
thetinahang <thetinahang@gmail.com>
2020-03-24 20:03:14 -0700

    fix only typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
-  resources :registrations, only[:create]
+  resources :registrations, only: [:create]
   delete :logout, to: 'session#logout'
   get :logged_in, to: 'sessions#logged_in'
 
