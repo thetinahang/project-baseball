f4ec888dcc75f4c424abc6c18093690a45a51fa2
thetinahang <thetinahang@gmail.com>
2020-03-29 20:38:23 -0700

    get login working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,74 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
  
-class Login extends React.Component {
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    const { email, password } = this.state;
+
+    axios
+      .post(
+        "http://localhost:3001/sessions",
+        {
+          user: {
+            email: email,
+            password: password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.logged_in) {
+          this.props.handleSuccessAuth(response.data);
+        }
+      })
+      .catch(error => {
+        console.log("login error", error);
+      });
+  }
+
   render() {
     return (
-      <form>
-        <h1>Login</h1>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <input type="submit" value="Login" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="email" 
+            name="email" 
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required 
+          />
+          <input 
+            type="password" 
+            name="password" 
+            placeholder="Password"
+            value={this.state.passwprd}
+            onChange={this.handleChange}
+            required 
+          />
+          
+          <button type="submit">Login</button>
+        </form>
+      </div>
     );
   }
 }
- 
-export default Login;
