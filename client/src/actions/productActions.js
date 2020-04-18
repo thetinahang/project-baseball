65ea96388aaaecd7193797ed71f852e8f234f2c6
thetinahang <thetinahang@gmail.com>
2020-04-18 16:21:18 -0700

    add post action to api

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -14,3 +14,34 @@ export const fetchProducts = () => {
     //then(products => {dispatch({ type: 'FETCH_PRODUCTS', products: products })})
   }
 } 
+
+
+export const addProduct = (product) => {
+  console.log(createAddUrl({id:1}))
+  let body = JSON.stringify(
+    {
+      product: {
+        name: product.name, 
+        description: product.description, 
+        user_id: product.user_id
+      }
+    }
+  )
+
+  return (dispatch) => {
+    dispatch({ type: 'ADD_PRODUCT' })
+    fetch(createUrl({id:1},
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: body
+      }
+    ))
+      .then(response => response.json())
+      //.then(json => console.log(json['data']))
+      .catch(error => console.log(error))
+    //then(products => {dispatch({ type: 'FETCH_PRODUCTS', products: products })})
+  }
+} 
