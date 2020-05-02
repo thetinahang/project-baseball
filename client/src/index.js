d2da976bad650e7e101c52c4212bf99e02e7812d
thetinahang <thetinahang@gmail.com>
2020-05-02 12:12:18 -0700

    remove navbar from index, keep it clean

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import KeyTermList from './containers/KeyTermList';
-import ProductsContainer from './containers/ProductsContainer';
-import Logout from './components/auth/Logout';
-import NavBar from './components/NavBar';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const Root = ({ store }) => (
+ReactDOM.render(
   <Provider store={store}>
-    <Router>
-	    <NavBar />
-	    <Route exact path="/" component={App} />
-	    <Route exact path="/products" component={ProductsContainer} />
-	    <Route exact path="/keyterms" component={KeyTermList} />
-	    <Route exact path="/logout" component={Logout} />
-    </Router>
-  </Provider>
-)
+    <App />
+  </Provider>, document.getElementById('root')
+);
 
-render(<Root store={store} />, document.getElementById('root'))
-
-export default Root;
