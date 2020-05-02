a2b99bb5e2a5af15d90bb45478f082d09ba4e0eb
thetinahang <thetinahang@gmail.com>
2020-05-02 11:20:14 -0700

    add remove product action

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,16 +1,16 @@
-export const fetchProducts = (user_id) => {
+export const fetchProducts = (userId) => {
     return (dispatch) => {
       dispatch({ type: 'LOADING_PRODUCTS' })
-      fetch(`http://localhost:3001/api/users/${user_id}/products`)
+      fetch(`http://localhost:3001/api/users/${userId}/products`)
         .then(response => response.json())
         .then(json => dispatch({ type: 'FETCH_PRODUCTS', products: json['data'] }))
         .catch(error => console.log(error))
     }
 }
 
-export const addProduct = (user_id, product) => {
+export const addProduct = (userId, product) => {
   return dispatch => {
-    fetch(`http://localhost:3001/api/users/${user_id}/products`, {
+    fetch(`http://localhost:3001/api/users/${userId}/products`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -21,3 +21,22 @@ export const addProduct = (user_id, product) => {
       .then(json => dispatch({type: 'ADD_PRODUCT', product: json}));
   }
 };
+
+export const removeProduct = (userId, productId) => {
+  return dispatch => {
+    dispatch({
+      type: 'REMOVE_PRODUCT',
+      userId: userId
+      productId: productId
+    });
+  fetch(`http://localhost:3001/api/users/${userId}/products/${productId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(productId)
+  }).then(resp => resp.json())
+    .then(json => console.log('deleted product from database', json));
+  }
+}
