e6a57d3b0f7a3aa7c5f17eef30ce37fd227dddd0
thetinahang <thetinahang@gmail.com>
2020-05-02 11:16:41 -0700

    clean up fetch products action

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,19 +1,12 @@
-function createUrl(params) {
-	return `http://localhost:3001/api/users/${params.id}/products`
+export const fetchProducts = (user_id) => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_PRODUCTS' })
+      fetch(`http://localhost:3001/api/users/${user_id}/products`)
+        .then(response => response.json())
+        .then(json => dispatch({ type: 'FETCH_PRODUCTS', products: json['data'] }))
+        .catch(error => console.log(error))
+    }
 }
-
-export const fetchProducts = () => {
-	console.log(createUrl({id:1}))
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_PRODUCTS' })
-    fetch(createUrl({id:1}))
-      .then(response => response.json())
-      //.then(json => console.log(json['data']))
-      .then(json => dispatch({ type: 'FETCH_PRODUCTS', products: json['data'] }))
-      .catch(error => console.log(error))
-    //then(products => {dispatch({ type: 'FETCH_PRODUCTS', products: products })})
-  }
-}; 
 
 export const addProduct = (user_id, product) => {
   return dispatch => {
