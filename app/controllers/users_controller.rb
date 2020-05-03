a12623fbee275e52160cbc3aa81dacc1b5db9205
thetinahang <thetinahang@gmail.com>
2020-05-03 13:21:44 -0700

    more experimentation with jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,39 @@
 class UsersController < ApplicationController
-	def find
-	   @user = User.find_by(email: params[:user][:email])
-	   if @user
-	     render json: @user
-	   else
-	     @errors = @user.errors.full_messages
-	     render json: @errors
-	   end
+	skip_before_action :require_login, only: [:create]
+
+#	def find
+#	   @user = User.find_by(email: params[:user][:email])
+#	   if @user
+#	     render json: @user
+#	   else
+#	     @errors = @user.errors.full_messages
+#	     render json: @errors
+#	   end
+#	end
+#	
+#	def show
+#		@user = User.find(params[:id]) 
+##		if session[:current_user_id] == @user.id
+#			render json: UserSerializer.new(@user)
+##		else
+##			render json: { errors: "Not authorized" }
+##		end
+#	end
+
+	def create
+		user = User.create(user_params)
+		if user.valid?
+			payload = {user_id: user.id}
+			token = encode_token(payload)
+			render json: {user: user, jwt: token}
+		else
+			render json: {errors: user.errors.full_messages}, status: :not_acceptable
+		end
 	end
-	
-	def show
-		@user = User.find(params[:id]) 
-#		if session[:current_user_id] == @user.id
-			render json: UserSerializer.new(@user)
-#		else
-#			render json: { errors: "Not authorized" }
-#		end
+
+	private
+
+	def user_params
+		params.permit(:email, :password)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,24 @@ Rails.application.routes.draw do
   # resources :key_terms
   # resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: 'sessions#logout'
-  get :logged_in, to: 'sessions#logged_in'
+#  resources :sessions, only: [:create]
+#  resources :registrations, only: [:create]
+#  delete :logout, to: 'sessions#logout'
+#  get :logged_in, to: 'sessions#logged_in'
 
-  scope '/api' do
+#  scope '/api' do
   	resources :users do 
 	  	resources :products do
 	  		resources :key_terms
         resources :product_prices
 	  	end
     end
-    post 'user_token', to: 'user_token#create'
-    post 'find_user', to: 'users#find'
-  end
+#    post 'user_token', to: 'user_token#create'
+#    post 'find_user', to: 'users#find'
+#  end
+
+  # resource :users, only: [:create]
+  post "/login", to: "auth#login"
+  get "/auto_login", to: "auth#auto_login"
+  get "/user_is_authed", to: "auth#user_is_authed"
 end
