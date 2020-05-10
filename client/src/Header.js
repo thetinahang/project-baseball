fc6d9064775709d629a3e75e0b0d506ec4446119
thetinahang <thetinahang@gmail.com>
2020-05-09 18:29:01 -0700

    Change header attributes

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const headerStyle = {
-    background: "black",
+    background: "green",
     height: "15vh",
     // lineHeight: "15vh"
 }
 function Header(props){
     return(
         <div style={headerStyle}>
-            <h1 style={{color: "white"}}>JWT Auth Setup</h1>
+            <h1 style={{color: "white"}}>Project Baseball</h1>
             <button className="ui button" onClick={() => props.handleFormSwitch("signUp")}>Sign Up</button>
             <button className="ui button" onClick={() => props.handleFormSwitch("login")}>Log In</button>
         </div>
